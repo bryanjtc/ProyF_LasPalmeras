a2ce697bd51bc4e52137704459ce0990de678fc9
Tonio <aqiu@ayudinga.org>
2021-07-23 03:38:40 -0500

    actualizacion frm restaurante

--- frmAdminRestaurantes.Designer.vb
+++ frmAdminRestaurantes.Designer.vb
@@ -22,37 +22,78 @@ Partial Class frmAdminRestaurantes
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.dgvRestaurantes = New System.Windows.Forms.DataGridView()
+        Me.IdsucursalDataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NombreDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.RestauranteBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.LasPalmerasDataSet = New ProyF_LasPalmeras.lasPalmerasDataSet()
         Me.lblRestauranteID = New System.Windows.Forms.Label()
         Me.lblNombre = New System.Windows.Forms.Label()
-        Me.lblTelefono1 = New System.Windows.Forms.Label()
-        Me.lblTelefono2 = New System.Windows.Forms.Label()
         Me.txtRestaurantID = New System.Windows.Forms.TextBox()
         Me.txtNombreRest = New System.Windows.Forms.TextBox()
-        Me.txtTele1 = New System.Windows.Forms.TextBox()
-        Me.txtTele2 = New System.Windows.Forms.TextBox()
+        Me.TelefonoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.dgvTelefonos = New System.Windows.Forms.DataGridView()
+        Me.IdsucursalDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TelefonoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnAgregarRest = New System.Windows.Forms.Button()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnVerTelefonos = New System.Windows.Forms.Button()
+        Me.btnTeleAdd = New System.Windows.Forms.Button()
+        Me.TelefonoTableAdapter = New ProyF_LasPalmeras.lasPalmerasDataSetTableAdapters.TelefonoTableAdapter()
+        Me.RestauranteTableAdapter = New ProyF_LasPalmeras.lasPalmerasDataSetTableAdapters.RestauranteTableAdapter()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.cboID = New System.Windows.Forms.ComboBox()
+        Me.RestauranteBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
         CType(Me.dgvRestaurantes, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RestauranteBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LasPalmerasDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TelefonoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.dgvTelefonos, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RestauranteBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgvRestaurantes
         '
+        Me.dgvRestaurantes.AutoGenerateColumns = False
+        Me.dgvRestaurantes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
         Me.dgvRestaurantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvRestaurantes.Location = New System.Drawing.Point(291, 12)
+        Me.dgvRestaurantes.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdsucursalDataGridViewTextBoxColumn1, Me.NombreDataGridViewTextBoxColumn})
+        Me.dgvRestaurantes.DataSource = Me.RestauranteBindingSource
+        Me.dgvRestaurantes.Location = New System.Drawing.Point(30, 12)
         Me.dgvRestaurantes.Name = "dgvRestaurantes"
-        Me.dgvRestaurantes.Size = New System.Drawing.Size(261, 214)
+        Me.dgvRestaurantes.Size = New System.Drawing.Size(798, 121)
         Me.dgvRestaurantes.TabIndex = 0
+        '
+        'IdsucursalDataGridViewTextBoxColumn1
+        '
+        Me.IdsucursalDataGridViewTextBoxColumn1.DataPropertyName = "id_sucursal"
+        Me.IdsucursalDataGridViewTextBoxColumn1.HeaderText = "id_sucursal"
+        Me.IdsucursalDataGridViewTextBoxColumn1.Name = "IdsucursalDataGridViewTextBoxColumn1"
+        '
+        'NombreDataGridViewTextBoxColumn
+        '
+        Me.NombreDataGridViewTextBoxColumn.DataPropertyName = "nombre"
+        Me.NombreDataGridViewTextBoxColumn.HeaderText = "nombre"
+        Me.NombreDataGridViewTextBoxColumn.Name = "NombreDataGridViewTextBoxColumn"
+        '
+        'RestauranteBindingSource
+        '
+        Me.RestauranteBindingSource.DataMember = "Restaurante"
+        Me.RestauranteBindingSource.DataSource = Me.LasPalmerasDataSet
+        '
+        'LasPalmerasDataSet
+        '
+        Me.LasPalmerasDataSet.DataSetName = "lasPalmerasDataSet"
+        Me.LasPalmerasDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'lblRestauranteID
         '
         Me.lblRestauranteID.AutoSize = True
-        Me.lblRestauranteID.Location = New System.Drawing.Point(56, 50)
+        Me.lblRestauranteID.Location = New System.Drawing.Point(36, 172)
         Me.lblRestauranteID.Name = "lblRestauranteID"
         Me.lblRestauranteID.Size = New System.Drawing.Size(79, 13)
         Me.lblRestauranteID.TabIndex = 1
@@ -61,135 +102,180 @@ Partial Class frmAdminRestaurantes
         'lblNombre
         '
         Me.lblNombre.AutoSize = True
-        Me.lblNombre.Location = New System.Drawing.Point(12, 95)
+        Me.lblNombre.Location = New System.Drawing.Point(227, 172)
         Me.lblNombre.Name = "lblNombre"
         Me.lblNombre.Size = New System.Drawing.Size(122, 13)
         Me.lblNombre.TabIndex = 2
         Me.lblNombre.Text = "Nombre del Restaurante"
         '
-        'lblTelefono1
-        '
-        Me.lblTelefono1.AutoSize = True
-        Me.lblTelefono1.Location = New System.Drawing.Point(76, 137)
-        Me.lblTelefono1.Name = "lblTelefono1"
-        Me.lblTelefono1.Size = New System.Drawing.Size(58, 13)
-        Me.lblTelefono1.TabIndex = 3
-        Me.lblTelefono1.Text = "Telefono 1"
-        '
-        'lblTelefono2
-        '
-        Me.lblTelefono2.AutoSize = True
-        Me.lblTelefono2.Location = New System.Drawing.Point(76, 179)
-        Me.lblTelefono2.Name = "lblTelefono2"
-        Me.lblTelefono2.Size = New System.Drawing.Size(58, 13)
-        Me.lblTelefono2.TabIndex = 4
-        Me.lblTelefono2.Text = "Telefono 2"
-        '
         'txtRestaurantID
         '
-        Me.txtRestaurantID.Location = New System.Drawing.Point(141, 47)
+        Me.txtRestaurantID.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.RestauranteBindingSource, "id_sucursal", True))
+        Me.txtRestaurantID.Location = New System.Drawing.Point(121, 169)
         Me.txtRestaurantID.Name = "txtRestaurantID"
         Me.txtRestaurantID.Size = New System.Drawing.Size(100, 20)
         Me.txtRestaurantID.TabIndex = 5
         '
         'txtNombreRest
         '
-        Me.txtNombreRest.Location = New System.Drawing.Point(140, 92)
+        Me.txtNombreRest.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.RestauranteBindingSource, "nombre", True))
+        Me.txtNombreRest.Location = New System.Drawing.Point(355, 169)
         Me.txtNombreRest.Name = "txtNombreRest"
-        Me.txtNombreRest.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombreRest.Size = New System.Drawing.Size(314, 20)
         Me.txtNombreRest.TabIndex = 6
         '
-        'txtTele1
+        'TelefonoBindingSource
         '
-        Me.txtTele1.Location = New System.Drawing.Point(141, 134)
-        Me.txtTele1.Name = "txtTele1"
-        Me.txtTele1.Size = New System.Drawing.Size(100, 20)
-        Me.txtTele1.TabIndex = 7
-        '
-        'txtTele2
-        '
-        Me.txtTele2.Location = New System.Drawing.Point(141, 176)
-        Me.txtTele2.Name = "txtTele2"
-        Me.txtTele2.Size = New System.Drawing.Size(100, 20)
-        Me.txtTele2.TabIndex = 8
+        Me.TelefonoBindingSource.DataMember = "Telefono"
+        Me.TelefonoBindingSource.DataSource = Me.LasPalmerasDataSet
         '
         'dgvTelefonos
         '
+        Me.dgvTelefonos.AutoGenerateColumns = False
+        Me.dgvTelefonos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
         Me.dgvTelefonos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvTelefonos.Location = New System.Drawing.Point(291, 281)
+        Me.dgvTelefonos.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdsucursalDataGridViewTextBoxColumn, Me.TelefonoDataGridViewTextBoxColumn})
+        Me.dgvTelefonos.DataSource = Me.TelefonoBindingSource
+        Me.dgvTelefonos.Location = New System.Drawing.Point(290, 216)
         Me.dgvTelefonos.Name = "dgvTelefonos"
-        Me.dgvTelefonos.Size = New System.Drawing.Size(261, 143)
+        Me.dgvTelefonos.Size = New System.Drawing.Size(252, 210)
         Me.dgvTelefonos.TabIndex = 9
+        '
+        'IdsucursalDataGridViewTextBoxColumn
+        '
+        Me.IdsucursalDataGridViewTextBoxColumn.DataPropertyName = "id_sucursal"
+        Me.IdsucursalDataGridViewTextBoxColumn.HeaderText = "id_sucursal"
+        Me.IdsucursalDataGridViewTextBoxColumn.Name = "IdsucursalDataGridViewTextBoxColumn"
+        '
+        'TelefonoDataGridViewTextBoxColumn
+        '
+        Me.TelefonoDataGridViewTextBoxColumn.DataPropertyName = "telefono"
+        Me.TelefonoDataGridViewTextBoxColumn.HeaderText = "telefono"
+        Me.TelefonoDataGridViewTextBoxColumn.Name = "TelefonoDataGridViewTextBoxColumn"
+        '
+        'btnAgregarRest
+        '
+        Me.btnAgregarRest.Location = New System.Drawing.Point(30, 139)
+        Me.btnAgregarRest.Name = "btnAgregarRest"
+        Me.btnAgregarRest.Size = New System.Drawing.Size(153, 23)
+        Me.btnAgregarRest.TabIndex = 10
+        Me.btnAgregarRest.Text = "Agregar Restaurante"
+        Me.btnAgregarRest.UseVisualStyleBackColor = True
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Location = New System.Drawing.Point(196, 139)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(153, 23)
+        Me.btnBorrar.TabIndex = 11
+        Me.btnBorrar.Text = "Borrar Restaurante"
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(675, 169)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(153, 23)
+        Me.btnGuardar.TabIndex = 12
+        Me.btnGuardar.Text = "Guardar Restaurante"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(66, 370)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(153, 23)
+        Me.btnSalir.TabIndex = 13
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'btnVerTelefonos
+        '
+        Me.btnVerTelefonos.Location = New System.Drawing.Point(30, 255)
+        Me.btnVerTelefonos.Name = "btnVerTelefonos"
+        Me.btnVerTelefonos.Size = New System.Drawing.Size(103, 23)
+        Me.btnVerTelefonos.TabIndex = 14
+        Me.btnVerTelefonos.Text = "Ver Telefonos"
+        Me.btnVerTelefonos.UseVisualStyleBackColor = True
+        '
+        'btnTeleAdd
+        '
+        Me.btnTeleAdd.Location = New System.Drawing.Point(139, 255)
+        Me.btnTeleAdd.Name = "btnTeleAdd"
+        Me.btnTeleAdd.Size = New System.Drawing.Size(110, 23)
+        Me.btnTeleAdd.TabIndex = 16
+        Me.btnTeleAdd.Text = "Agregar Telefono"
+        Me.btnTeleAdd.UseVisualStyleBackColor = True
+        '
+        'TelefonoTableAdapter
+        '
+        Me.TelefonoTableAdapter.ClearBeforeFill = True
+        '
+        'RestauranteTableAdapter
+        '
+        Me.RestauranteTableAdapter.ClearBeforeFill = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 219)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "ID Seleccionado"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(58, 222)
+        Me.Button1.Location = New System.Drawing.Point(64, 341)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(153, 23)
-        Me.Button1.TabIndex = 10
-        Me.Button1.Text = "Agregar Restaurante"
+        Me.Button1.Size = New System.Drawing.Size(155, 23)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Button2
+        'cboID
         '
-        Me.Button2.Location = New System.Drawing.Point(58, 252)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(153, 23)
-        Me.Button2.TabIndex = 11
-        Me.Button2.Text = "Borrar Restaurante"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.cboID.DataSource = Me.RestauranteBindingSource1
+        Me.cboID.DisplayMember = "id_sucursal"
+        Me.cboID.FormattingEnabled = True
+        Me.cboID.Location = New System.Drawing.Point(128, 216)
+        Me.cboID.Name = "cboID"
+        Me.cboID.Size = New System.Drawing.Size(121, 21)
+        Me.cboID.TabIndex = 21
         '
-        'Button3
+        'RestauranteBindingSource1
         '
-        Me.Button3.Location = New System.Drawing.Point(58, 281)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(153, 23)
-        Me.Button3.TabIndex = 12
-        Me.Button3.Text = "Guardar Data"
-        Me.Button3.UseVisualStyleBackColor = True
-        '
-        'Button4
-        '
-        Me.Button4.Location = New System.Drawing.Point(58, 339)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(153, 23)
-        Me.Button4.TabIndex = 13
-        Me.Button4.Text = "Salir"
-        Me.Button4.UseVisualStyleBackColor = True
-        '
-        'Button5
-        '
-        Me.Button5.Location = New System.Drawing.Point(58, 310)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(153, 23)
-        Me.Button5.TabIndex = 14
-        Me.Button5.Text = "Ver Telefonos"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.RestauranteBindingSource1.DataMember = "Restaurante"
+        Me.RestauranteBindingSource1.DataSource = Me.LasPalmerasDataSet
         '
         'frmAdminRestaurantes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(564, 450)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
+        Me.ClientSize = New System.Drawing.Size(840, 450)
+        Me.Controls.Add(Me.cboID)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnTeleAdd)
+        Me.Controls.Add(Me.btnVerTelefonos)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.btnAgregarRest)
         Me.Controls.Add(Me.dgvTelefonos)
-        Me.Controls.Add(Me.txtTele2)
-        Me.Controls.Add(Me.txtTele1)
         Me.Controls.Add(Me.txtNombreRest)
         Me.Controls.Add(Me.txtRestaurantID)
-        Me.Controls.Add(Me.lblTelefono2)
-        Me.Controls.Add(Me.lblTelefono1)
         Me.Controls.Add(Me.lblNombre)
         Me.Controls.Add(Me.lblRestauranteID)
         Me.Controls.Add(Me.dgvRestaurantes)
         Me.Name = "frmAdminRestaurantes"
         Me.Text = "frmAdminRestaurantes"
         CType(Me.dgvRestaurantes, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RestauranteBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LasPalmerasDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TelefonoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.dgvTelefonos, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RestauranteBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -198,16 +284,26 @@ Partial Class frmAdminRestaurantes
     Friend WithEvents dgvRestaurantes As DataGridView
     Friend WithEvents lblRestauranteID As Label
     Friend WithEvents lblNombre As Label
-    Friend WithEvents lblTelefono1 As Label
-    Friend WithEvents lblTelefono2 As Label
     Friend WithEvents txtRestaurantID As TextBox
     Friend WithEvents txtNombreRest As TextBox
-    Friend WithEvents txtTele1 As TextBox
-    Friend WithEvents txtTele2 As TextBox
     Friend WithEvents dgvTelefonos As DataGridView
+    Friend WithEvents btnAgregarRest As Button
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnVerTelefonos As Button
+    Friend WithEvents btnTeleAdd As Button
+    Friend WithEvents LasPalmerasDataSet As lasPalmerasDataSet
+    Friend WithEvents TelefonoBindingSource As BindingSource
+    Friend WithEvents TelefonoTableAdapter As lasPalmerasDataSetTableAdapters.TelefonoTableAdapter
+    Friend WithEvents IdsucursalDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents TelefonoDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents RestauranteBindingSource As BindingSource
+    Friend WithEvents RestauranteTableAdapter As lasPalmerasDataSetTableAdapters.RestauranteTableAdapter
+    Friend WithEvents IdsucursalDataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents NombreDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
+    Friend WithEvents cboID As ComboBox
+    Friend WithEvents RestauranteBindingSource1 As BindingSource
 End Class
--- frmAdminRestaurantes.vb
+++ frmAdminRestaurantes.vb
@@ -1,5 +1,10 @@
 ﻿Public Class frmAdminRestaurantes
     Private Sub frmAdminRestaurantes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'LasPalmerasDataSet.Pedido' table. You can move, or remove it, as needed.
+        'TODO: This line of code loads data into the 'LasPalmerasDataSet.Restaurante' table. You can move, or remove it, as needed.
+        Me.RestauranteTableAdapter.Fill(Me.LasPalmerasDataSet.Restaurante)
+        'TODO: This line of code loads data into the 'LasPalmerasDataSet.Telefono' table. You can move, or remove it, as needed.
+        Me.TelefonoTableAdapter.Fill(Me.LasPalmerasDataSet.Telefono)
 
 
     End Sub
@@ -7,4 +12,39 @@
     Private Sub dgvRestaurantes_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvRestaurantes.CellContentClick
 
     End Sub
+
+    Private Sub btnAgregarRest_Click(sender As Object, e As EventArgs) Handles btnAgregarRest.Click
+        RestauranteBindingSource.AddNew()
+    End Sub
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        RestauranteBindingSource.RemoveCurrent()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        RestauranteBindingSource.EndEdit()
+        RestauranteTableAdapter.Update(LasPalmerasDataSet)
+
+    End Sub
+
+    Private Sub btnTeleAdd_Click(sender As Object, e As EventArgs) Handles btnTeleAdd.Click
+        Dim selected As String = InputBox("Restaurante", "indique el ID del Cliente")
+        Dim telefone As String = InputBox("Telefono", "Mete un telefono")
+        Dim newrow As DataRow = LasPalmerasDataSet.Tables("Telefono").NewRow()
+        newrow(0) = selected
+        newrow(1) = telefone
+        LasPalmerasDataSet.Telefono.Rows.Add(newrow)
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Me.Hide()
+        frmAdmin.Show()
+    End Sub
+
+    Private Sub btnVerTelefonos_Click(sender As Object, e As EventArgs) Handles btnVerTelefonos.Click
+        Dim s As String = cboID.Text
+        TelefonoBindingSource.Filter() = "id_sucursal= '" & s & "'"
+    End Sub
+
+
 End Class
