a0df2026163b8d2dad9c85ead9e146575631b368
Bryan Thomas <49354825+bryan250000@users.noreply.github.com>
2021-07-23 00:20:27 -0500

    Cambios a settings

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -51,18 +51,6 @@ Namespace My
                 End If
 #End If
                 Return defaultInstance
-            End Get
-        End Property
-        
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=tcp:herramientas.database.windows.net,1433;Initial Catalog=lasPalmera"& _ 
-            "s;Persist Security Info=True;User ID=bryan;Password=kpgA@F!obNqN6Grog2sP;Multipl"& _ 
-            "eActiveResultSets=False;Encrypt=True;TrustServerCertificate=False")>  _
-        Public ReadOnly Property LasPalmerasConnection() As String
-            Get
-                Return CType(Me("LasPalmerasConnection"),String)
             End Get
         End Property
         
