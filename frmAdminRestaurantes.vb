d7691206b27c9bf5128d269d9c99eb4e4ffb4c18
Tonio <aqiu@ayudinga.org>
2021-07-23 00:08:34 -0500

    Creacion de tabla de restaurantes

---
+++ frmAdminRestaurantes.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmAdminRestaurantes
+    Private Sub frmAdminRestaurantes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub dgvRestaurantes_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvRestaurantes.CellContentClick
+
+    End Sub
+End Class
