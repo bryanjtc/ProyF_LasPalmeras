130904a6fa47227a2b54f9c079e41b5e238618a5
Tonio <aqiu@ayudinga.org>
2021-07-23 01:10:35 -0500

    Creacion de form de motorizado

---
+++ frmAdminMotorizado.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAdminMotorizado
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.lblMotoID = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblListado = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.LasPalmerasDataSet = New ProyF_LasPalmeras.lasPalmerasDataSet()
+        Me.MotorizadoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.MotorizadoTableAdapter = New ProyF_LasPalmeras.lasPalmerasDataSetTableAdapters.MotorizadoTableAdapter()
+        Me.IdmotorizadoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NombreDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ApellidoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LasPalmerasDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MotorizadoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdmotorizadoDataGridViewTextBoxColumn, Me.NombreDataGridViewTextBoxColumn, Me.ApellidoDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.MotorizadoBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(204, 56)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(247, 313)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'lblMotoID
+        '
+        Me.lblMotoID.AutoSize = True
+        Me.lblMotoID.Location = New System.Drawing.Point(13, 70)
+        Me.lblMotoID.Name = "lblMotoID"
+        Me.lblMotoID.Size = New System.Drawing.Size(88, 13)
+        Me.lblMotoID.TabIndex = 1
+        Me.lblMotoID.Text = "ID de Motorizado"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(99, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nombre Motorizado"
+        '
+        'lblListado
+        '
+        Me.lblListado.AutoSize = True
+        Me.lblListado.Location = New System.Drawing.Point(201, 40)
+        Me.lblListado.Name = "lblListado"
+        Me.lblListado.Size = New System.Drawing.Size(111, 13)
+        Me.lblListado.TabIndex = 3
+        Me.lblListado.Text = "Listado de Motorizado"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(98, 67)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(98, 103)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(31, 223)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Agregar Motorizado"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(31, 252)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(121, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Guardar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(31, 281)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(121, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Despedir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(31, 311)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(121, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Salir"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'LasPalmerasDataSet
+        '
+        Me.LasPalmerasDataSet.DataSetName = "lasPalmerasDataSet"
+        Me.LasPalmerasDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'MotorizadoBindingSource
+        '
+        Me.MotorizadoBindingSource.DataMember = "Motorizado"
+        Me.MotorizadoBindingSource.DataSource = Me.LasPalmerasDataSet
+        '
+        'MotorizadoTableAdapter
+        '
+        Me.MotorizadoTableAdapter.ClearBeforeFill = True
+        '
+        'IdmotorizadoDataGridViewTextBoxColumn
+        '
+        Me.IdmotorizadoDataGridViewTextBoxColumn.DataPropertyName = "id_motorizado"
+        Me.IdmotorizadoDataGridViewTextBoxColumn.HeaderText = "id_motorizado"
+        Me.IdmotorizadoDataGridViewTextBoxColumn.Name = "IdmotorizadoDataGridViewTextBoxColumn"
+        '
+        'NombreDataGridViewTextBoxColumn
+        '
+        Me.NombreDataGridViewTextBoxColumn.DataPropertyName = "nombre"
+        Me.NombreDataGridViewTextBoxColumn.HeaderText = "nombre"
+        Me.NombreDataGridViewTextBoxColumn.Name = "NombreDataGridViewTextBoxColumn"
+        '
+        'ApellidoDataGridViewTextBoxColumn
+        '
+        Me.ApellidoDataGridViewTextBoxColumn.DataPropertyName = "apellido"
+        Me.ApellidoDataGridViewTextBoxColumn.HeaderText = "apellido"
+        Me.ApellidoDataGridViewTextBoxColumn.Name = "ApellidoDataGridViewTextBoxColumn"
+        '
+        'frmAdminMotorizado
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(463, 401)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lblListado)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblMotoID)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "frmAdminMotorizado"
+        Me.Text = "frmAdminMotorizado"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LasPalmerasDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MotorizadoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents lblMotoID As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblListado As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents LasPalmerasDataSet As lasPalmerasDataSet
+    Friend WithEvents MotorizadoBindingSource As BindingSource
+    Friend WithEvents MotorizadoTableAdapter As lasPalmerasDataSetTableAdapters.MotorizadoTableAdapter
+    Friend WithEvents IdmotorizadoDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents NombreDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents ApellidoDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+End Class
---
+++ frmAdminMotorizado.vb
@@ -0,0 +1,7 @@
+﻿Public Class frmAdminMotorizado
+    Private Sub frmAdminMotorizado_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'LasPalmerasDataSet.Motorizado' table. You can move, or remove it, as needed.
+        Me.MotorizadoTableAdapter.Fill(Me.LasPalmerasDataSet.Motorizado)
+
+    End Sub
+End Class
